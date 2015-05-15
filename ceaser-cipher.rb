77a2ad73ed47aa7a310be557cfc088979a9395e8
Robin Unger <robin.e.unger@gmail.com>
2015-05-15 15:50:26 -0600

    implemented ceaser cipher encyption. preserves case, character wrapping

---
+++ ceaser-cipher.rb
@@ -0,0 +1,40 @@
+def encrypt(text, key)
+    encrypted_text = ""
+
+    text.each_char do |char|
+        if char.match(/[[:alpha:]]/)
+            encrypted_text << shift_character(char, key)
+        else
+            encrypted_text << char
+        end
+    end
+
+    encrypted_text
+end
+
+
+def decrypt(text, key)
+end
+
+
+def shift_character(char, shift_factor)
+    char_value = char.ord
+    # first and last character ordinates, assuming char is lowercase
+    wrap_start = "a".ord
+    wrap_end = "z".ord
+
+    # reassign first, last char ordinates if char is uppercase
+    wrap_start, wrap_end = "A".ord, "Z".ord if char.match(/[[:upper:]]/)
+
+    while shift_factor > 0 do
+        char_value == wrap_end ? char_value = wrap_start : char_value += 1
+        shift_factor -= 1
+    end
+
+    char_value.chr
+end
+
+
+puts encrypt("Nothing to see here. The password is 2099", 100)
+puts encrypt("x y.z!", 100)
+puts encrypt("X Y.Z!", 100)
