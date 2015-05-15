a8afc13247fc053596ec44832e1a4c9acd28497f
Robin Unger <robin.e.unger@gmail.com>
2015-05-15 16:32:13 -0600

    encryption to accept negative key values

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -12,11 +12,6 @@ def encrypt(text, key)
     encrypted_text
 end
 
-
-def decrypt(text, key)
-end
-
-
 def shift_character(char, shift_factor)
     char_value = char.ord
     # first and last character ordinates, assuming char is lowercase
@@ -26,9 +21,16 @@ def shift_character(char, shift_factor)
     # reassign first, last char ordinates if char is uppercase
     wrap_start, wrap_end = "A".ord, "Z".ord if char.match(/[[:upper:]]/)
 
-    while shift_factor > 0 do
-        char_value == wrap_end ? char_value = wrap_start : char_value += 1
-        shift_factor -= 1
+    if shift_factor < 0
+        while shift_factor != 0 do
+            char_value == wrap_start ? char_value = wrap_end : char_value -= 1
+            shift_factor += 1
+        end
+    else
+        while shift_factor != 0 do
+            char_value == wrap_end ? char_value = wrap_start : char_value += 1
+            shift_factor -= 1
+        end
     end
 
     char_value.chr
@@ -38,3 +40,6 @@ end
 puts encrypt("Nothing to see here. The password is 2099", 100)
 puts encrypt("x y.z!", 100)
 puts encrypt("X Y.Z!", 100)
+puts encrypt("Nothing to see here. The password is 2099", -100)
+puts encrypt("a b.c!", -100)
+puts encrypt("A B.C!", -100)
